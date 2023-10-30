fbfa3d2bdff43274f213c4219960f4142b210cf6
Facundo Lucero <facundon.lucero@alumnos.frm.utn.edu.ar>
2023-10-30 14:57:27 -0300

    feat: first test

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,10 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (sorted', elem') where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+elem' :: (Eq a) => [a] -> a -> Bool
+elem' [] _ = False
+elem' (x : xs) y = x == y || elem' xs y
 
+sorted' :: (Ord a) => [a] -> Bool
+sorted' [] = True
+sorted' [_] = True
+sorted' (h : n : xs) = h < n && sorted' xs
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,27 @@
+import Lib
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "elem'" $ do
+    it "returns true if element is in the list" $ do
+      elem' [1, 65, 123, 12, 11, 12] 12 `shouldBe` True
+
+    it "returns false if element is not in the list" $ do
+      elem' [1, 65, 123, 12, 11, 12] 99 `shouldBe` False
+
+    it "returns false if the list is empty" $ do
+      elem' [] 99 `shouldBe` False
+
+  describe "sorted" $ do
+    it "returns true if the list is sorted" $ do
+      sorted' [1, 2, 3, 7] `shouldBe` True
+
+    it "returns false if the list is not sorted" $ do
+      sorted' [1, 2, 7, 4] `shouldBe` False
+
+    it "returns true if the list is empty" $ do
+      sorted' ([] :: [Int]) `shouldBe` True
+
+    it "returns true if the list have one elem" $ do
+      sorted' [10] `shouldBe` True
